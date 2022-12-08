f83db4083585fdd2e1ccf2f18a857451971d2fa2
Esti-Vana <esterv9413@gmail.com>
2022-12-09 01:39:25 +0200

    second-upload

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use App\Messages;
 Route::get('/login',function(){
     return view('login');
 });
+
 Route::get('/dashboard',function(){
      $msgs=Messages::AllMsg();
-   
-    return view('dashboard',['msgs'=>$msgs]);   
+     return view('dashboard',['msgs'=>$msgs]);   
 });
 
 Route::get('/authenticate',function(){
-//dd($request->all());      
-$username = htmlspecialchars($_GET['username']);
-$userpassword  = htmlspecialchars($_GET['userpassword']);
+   //dd($request->all());      
+   $username = htmlspecialchars($_GET['username']);
+   $userpassword  = htmlspecialchars($_GET['userpassword']);
    if(Users::Find($username,$userpassword)==200)
    {     
       header("HTTP/1.1 200 OK");
@@ -34,6 +34,4 @@ $userpassword  = htmlspecialchars($_GET['userpassword']);
       header("Status: 401 Not Found");
       return "401 Not Found";
    }
-
 });
-
